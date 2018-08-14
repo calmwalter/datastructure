74a34b199177abce82bf9d5a4f8ea2b26482ea5f
calmwalter <calmwalter@outlook.com>
2018-08-14 10:11:50 +0800

    我终于修复了RBTREE的所有bug

--- RBtree.c
+++ RBtree.c
@@ -12,27 +12,39 @@ struct NODE
 struct RBTREE
 {
     struct NODE *root;
+    struct NODE *NIL;
 };
 
-void BUILD_TREE(struct RBTREE *rbtree)
+void BUILD_TREE(struct RBTREE *T)
 {
-    rbtree->root = NULL;
+
+    T->NIL = (struct NODE *)malloc(sizeof(struct NODE));
+    T->NIL->color = 1;
+    T->NIL->left = NULL;
+    T->NIL->right = NULL;
+    T->NIL->p = NULL;
+    T->NIL->key = -1;
+    T->root = T->NIL;
 }
 //左旋
 void LEFT_ROTATE(struct RBTREE *T, struct NODE *x)
 {
+
     struct NODE *y = x->right;
+    //第一层关系x与z
     //先把y的左边节点给x的右边节点
     x->right = y->left;
-    //改变它的父节点的指针
-    if (y->left != NULL)
+    //改变y左的父节点的指针
+    if (y->left != T->NIL)
     {
         y->left->p = x;
     }
-
+    //第二层关系y与x父节点
     //把y放到x原本的位置
+    //y父为x父
     y->p = x->p;
-    if (x->p == NULL)
+    //x父子为y
+    if (x->p == T->NIL)
     {
         T->root = y;
     }
@@ -44,6 +56,7 @@ void LEFT_ROTATE(struct RBTREE *T, struct NODE *x)
     {
         x->p->right = y;
     }
+    //第三层关系y与x
     //调整y的左子节点，和x的父节点
     y->left = x;
     x->p = y;
@@ -52,16 +65,17 @@ void LEFT_ROTATE(struct RBTREE *T, struct NODE *x)
 void RIGHT_ROTATE(struct RBTREE *T, struct NODE *y)
 {
     struct NODE *x = y->left;
+    //第一层关系
     //先把x的右节点给y的左节点
     y->left = x->right;
-    if (x->right != NULL)
+    if (x->right != T->NIL)
     {
         x->right->p = y;
     }
-
+    //第二层关系
     //再把x放到y原本的位置
     x->p = y->p;
-    if (y->p == NULL)
+    if (y->p == T->NIL)
     {
         T->root = x;
     }
@@ -73,6 +87,7 @@ void RIGHT_ROTATE(struct RBTREE *T, struct NODE *y)
     {
         y->p->right = x;
     }
+    //第三层关系
     //再调整x的右子节点和y的父节点
     x->right = y;
     y->p = x;
@@ -102,29 +117,32 @@ void RB_INSERT_FIXUP(struct RBTREE *T, struct NODE *z)
                 z = z->p;
                 LEFT_ROTATE(T, z);
             }
-            //如果是左孩子，先把z的父节点变成黑色，再把z的父节点的父节点变成红色，再进行右旋，情况3
-            z->p->color = 1;
-            z->p->p->color = 0;
-            RIGHT_ROTATE(T, z->p->p);
+            else
+            {
+                //如果是左孩子，先把z的父节点变成黑色，再把z的父节点的父节点变成红色，再进行右旋，情况3
+                z->p->color = 1;
+                z->p->p->color = 0;
+                RIGHT_ROTATE(T, z->p->p);
+            }
         }
         //如果这个节点在右边
         else
         {
-            struct NODE *y = z->p->left;
-            if (z->p->p->left == 0)
+            struct NODE *y = z->p->p->left;
+            if (y->color == 0)
             {
                 z->p->color = 1;
                 z->p->p->color = 0;
                 y->color = 1;
             }
-            else if (z == z->p->right)
+            else if (z == z->p->left)
             {
                 z = z->p;
-                LEFT_ROTATE(T, z);
+                RIGHT_ROTATE(T, z);
             }
             z->p->color = 1;
             z->p->p->color = 0;
-            RIGHT_ROTATE(T, z->p->p);
+            LEFT_ROTATE(T, z->p->p);
         }
     }
     T->root->color = 1;
@@ -132,14 +150,14 @@ void RB_INSERT_FIXUP(struct RBTREE *T, struct NODE *z)
 
 void RB_INSERT(struct RBTREE *T, struct NODE *z)
 {
-    struct NODE *y = NULL;
+    struct NODE *y = T->NIL;
     struct NODE *x = T->root;
-    while (x != NULL)
+    while (x != T->NIL)
     {
         //把传递给y
         y = x;
         //把x的关键值与z的关键字比较
-        if (x->key < z->key)
+        if (x->key > z->key)
         {
             x = x->left;
         }
@@ -151,7 +169,7 @@ void RB_INSERT(struct RBTREE *T, struct NODE *z)
     //把y的指针传递给z的p属性
     z->p = y;
     //把z传递给y的左边或者右边
-    if (y == NULL)
+    if (y == T->NIL)
     {
         T->root = z;
     }
@@ -163,13 +181,36 @@ void RB_INSERT(struct RBTREE *T, struct NODE *z)
     {
         y->right = z;
     }
-    z->left = NULL;
-    z->right = NULL;
+    z->left = T->NIL;
+    z->right = T->NIL;
     z->color = 0; //0代表红色
-    void RB_INSERT_FIXUP(T, z);
+    RB_INSERT_FIXUP(T, z);
+}
+void TREE_WALK(struct RBTREE *T, struct NODE *node)
+{
+    if (node == T->NIL)
+    {
+        return;
+    }
+    TREE_WALK(T, node->left);
+    printf("%d ", node->key);
+    TREE_WALK(T, node->right);
 }
 int main(int argc, char const *argv[])
 {
-    printf("hello world!");
+    struct RBTREE T;
+    BUILD_TREE(&T);
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < 10; i++)
+    {
+        struct NODE *n = (struct NODE *)malloc(sizeof(struct NODE));
+        n->color = 0;
+        n->key = rand() % 100 + 1;
+        n->left = T.NIL;
+        n->right = T.NIL;
+        n->p = T.NIL;
+        RB_INSERT(&T, n);
+    }
+    TREE_WALK(&T, T.root);
     return 0;
 }
